a7544c312d76fb09a04f755e9c94aa6c39971906
diomastrio <zhchypyp@gmail.com>
2025-05-03 18:04:46 -0600

    fix: standardize import statement for supabase client in authHooks

--- src/authentication/authHooks.js
+++ src/authentication/authHooks.js
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import React, { useEffect, useState } from "react"; // Import useEffect and useState
+import React, { useEffect, useState } from "react";
 import {
   login as loginApi,
   updateCurrentUser,
@@ -10,7 +10,7 @@ import {
   signInWithGoogle,
   createUserProfileIfNeeded,
 } from "../services/apiAuth";
-import { supabase } from "../services/supabaseClient"; // Import supabase client
+import supabase from "../services/supabaseClient";
 
 export function useLogin() {
   const queryClient = useQueryClient();
